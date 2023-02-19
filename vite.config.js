199642a5f37bcc5fe0ce6737ed8dce626fc18954
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-02-20 04:09:42 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -41,21 +41,21 @@ export default defineConfig({
                 ],
             },
             manifest: {
-                name: "Piped",
+                name: "VIVEKFY",
                 short_name: "Piped",
                 background_color: "#000000",
                 theme_color: "#fa4b4b",
                 icons: [
-                    { src: "./img/icons/android-chrome-192x192.png", sizes: "192x192", type: "image/png" },
-                    { src: "./img/icons/android-chrome-512x512.png", sizes: "512x512", type: "image/png" },
+                    { src: "./img/icons/vivek.png", sizes: "192x192", type: "image/png" },
+                    { src: "./img/icons/vivek.png", sizes: "512x512", type: "image/png" },
                     {
-                        src: "./img/icons/android-chrome-maskable-192x192.png",
+                        src: "./img/icons/vivek.png",
                         sizes: "192x192",
                         type: "image/png",
                         purpose: "maskable",
                     },
                     {
-                        src: "./img/icons/android-chrome-maskable-512x512.png",
+                        src: "./img/icons/vivek.png",
                         sizes: "512x512",
                         type: "image/png",
                         purpose: "maskable",
