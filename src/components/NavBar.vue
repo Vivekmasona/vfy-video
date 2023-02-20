87d3d27da4a0c810481081f6b6cb3b9da411e980
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-02-20 08:22:15 +0530

    Update NavBar.vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -8,7 +8,7 @@
                     height="32"
                     width="32"
                     class="w-10 mr-[-0.6rem]"
-                />VIVEKFY</router-link
+                />𝐕𝐢𝐯𝐞𝐤𝐟𝐲</router-link
             >
         </div>
         <div class="lt-md:hidden search-container">
